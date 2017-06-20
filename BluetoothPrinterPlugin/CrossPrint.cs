93741d3f5e3aaa5b90f16867f7306c3ac1f3b637
Tutu George <tutugeorge@gmail.com>
2017-06-21 00:03:17 +0530

    Added PCL abstractions

---
+++ BluetoothPrinterPlugin/CrossPrint.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XF.Plugin.Abstractions;
+
+namespace XF.Plugin
+{
+    public static class CrossPrint
+    {
+        static Lazy<IPrint> TTS = new Lazy<IPrint>(
+            () => CreatePrint(), 
+            System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+        public static IPrint Current
+        {
+            get
+            {
+                var ret = TTS.Value;
+                if (ret == null)
+                    throw NotImplementedInReferenceAssembly();
+                return ret; 
+            }
+        }
+
+        static IPrint CreatePrint()
+        {
+#if PORTABLE
+            return null;
+#else
+            return new Print();
+#endif
+        }
+
+        internal static Exception NotImplementedInReferenceAssembly()
+        {
+            return new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the Xam.Plugins.Vibrate NuGet package from your main application project in order to reference the platform-specific implementation.");
+        }
+    }
+}
---
+++ BluetoothPrinterPlugin/IPrint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF.Plugin.Abstractions
+{
+    public interface IPrint
+    {
+        /// <summary>
+        /// Print the input stirng to blue tooth printer
+        /// </summary>
+        /// <param name="input">input data in string format</param>
+        void Print(string input);
+    }
+}
